5c18c8cc1918d034973d30c2d475a7cd511be32f
Ariel <arielmich64@gmail.com>
2026-08-15 01:39:37 -0400

    change some hypr border settings

--- hypr/.config/hypr/config/decorations.lua
+++ hypr/.config/hypr/config/decorations.lua
@@ -3,7 +3,7 @@
 hl.config({
     general = {
         gaps_in = 3,
-        gaps_out = 8,
+        gaps_out = 4,
         border_size = 2,
         extend_border_grab_area = 10,
         resize_on_border = true,
